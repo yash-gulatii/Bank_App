1858a2a8b3e70e2e81bab1b8ea6ab1ca036ed579
Yash <contactyashgulati@gmail.com>
2023-01-06 00:23:33 +0530

    Final Build

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,27 @@ import {
   Footer,
 } from "./components";
 
-const App = () => {
-  return (
-    <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
-      </div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
-      </div>
-
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          Stats Business Billing CardDeal Testimonials Clients CTA Footer
-        </div>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
     </div>
-  );
-};
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />
+        <Clients /> <CTA /> <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
